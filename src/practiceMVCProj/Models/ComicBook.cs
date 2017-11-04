14d650d8c434f29c86d7c6f47b1534ec3899da66
Saray <Saray@SARAY>
2017-11-04 19:00:47 +0200

    Added comic book and artist models

---
+++ src/practiceMVCProj/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace practiceMVCProj.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
