9c079d3c9137f779528b6edf1d44068bb8bcd523
Saray <Saray@SARAY>
2017-11-04 15:43:33 +0200

    Added Comic Book Detail view

--- src/practiceMVCProj/Controllers/ComicBooksController.cs
+++ src/practiceMVCProj/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace practiceMVCProj.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/path");
-            }
-            return Content("Hello from the ComicBooksController!");
+            return View();
         }
     }
 }
