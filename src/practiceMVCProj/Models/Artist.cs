fb74b9cdb0034e2913d72aa59ec2d3942a05513d
Saray <Saray@SARAY>
2017-11-05 18:44:24 +0200

    Updated Comic Book Detail view to be strongly typed

--- src/practiceMVCProj/Models/Artist.cs
+++ src/practiceMVCProj/Models/Artist.cs
@@ -7,7 +7,7 @@ namespace practiceMVCProj.Models
 {
     public class Artist
     {
-        public string Name { get; set; }
         public string Role { get; set; }
+        public string Name { get; set; }
     }
 }
--- src/practiceMVCProj/Models/ComicBook.cs
+++ src/practiceMVCProj/Models/ComicBook.cs
@@ -10,7 +10,7 @@ namespace practiceMVCProj.Models
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public string Description { get; set; }
+        public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
