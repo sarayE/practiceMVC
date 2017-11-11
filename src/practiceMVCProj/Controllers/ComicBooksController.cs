f38b8cd7fb29f2315b0794a2f5ebaff31f3cd489
Saray <Saray@SARAY>
2017-11-11 13:48:06 +0200

    Added comic book repository

--- src/practiceMVCProj/Controllers/ComicBooksController.cs
+++ src/practiceMVCProj/Controllers/ComicBooksController.cs
@@ -11,21 +11,7 @@ namespace practiceMVCProj.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook1 = new ComicBook() {
-
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {  Role ="Script", Name= "Dan Slott" },
-                    new Artist() {  Role="Pencils", Name= "Humberto Ramos" },
-                    new Artist() {  Role="Inks", Name= "Victor Olazaba" },
-                    new Artist() {  Role="Colors", Name= "Edgar Delgado" },
-                    new Artist() {  Role="Letters", Name= "Chris Eliopoulos" },
-                }
-            };
-            return View(comicBook1);
+            return View();
         }
     }
 }
